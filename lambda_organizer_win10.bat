c29b98df8ac4d95142ab1b3926472f10936455ad
Jason Neale <40077498+JasonNeale@users.noreply.github.com>
2019-10-24 02:11:19 -0400

    Initial commit
    This create an organized file structure on your Desktop (only for Windows users, sorry Mac) for the Lambda Full Stack Web Development course

---
+++ lambda_organizer_win10.bat
@@ -0,0 +1,72 @@
+@echo off
+ 
+MkDir "%USERPROFILE%\Desktop\Lambda"
+MkDir "%USERPROFILE%\Desktop\Lambda\Course"
+MkDir "%USERPROFILE%\Desktop\Lambda\Notes"
+MkDir "%USERPROFILE%\Desktop\Lambda\Misc"
+
+MkDir "%USERPROFILE%\Desktop\Lambda\Course"
+
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\1. Lambda Launch"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\1. Lambda Launch\1. Lambda Orientation"
+
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\2. Web Fundamentals"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\2. Web Fundamentals\1. User Interface and Git"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\2. Web Fundamentals\2. Advanced CSS"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\2. Web Fundamentals\3. Javascript Fundamentals"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\2. Web Fundamentals\4. WEB Unit 1 Build"
+
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\3. Web Applications I"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\3. Web Applications I\1. Applied Javascript"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\3. Web Applications I\2. Intro to React"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\3. Web Applications I\3. Single Page Applications"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\3. Web Applications I\4. WEB Unit 2 Build"
+
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\4. Web Applications II"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\4. Web Applications II\1. Advanced React"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\4. Web Applications II\2. Advanced State Management"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\4. Web Applications II\3. Advanced Web Applications"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\4. Web Applications II\4. WEB Unit 3 Build"
+
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\5. Web API - Node"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\5. Web API - Node\1. Build a Web API"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\5. Web API - Node\2. Adding Data Persistance"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\5. Web API - Node\3. Authentication and Testing"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\5. Web API - Node\4. WEB Unit 4 Node Build"
+
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\6. Web API - Javas"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\6. Web API - Java\1. WEB Java Fundamentals"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\6. Web API - Java\2. RDBMS and API Introduction"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\6. Web API - Java\3. Java Fundamentals"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\6. Web API - Java\4. WEB Unit 4 Java Build"
+
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\7. Computer Science"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\7. Computer Science\1. Intro to Python and OOP"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\7. Computer Science\2. Algorithms"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\7. Computer Science\3. Data Structures"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\7. Computer Science\4. Hash Tables and Blockchain"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\7. Computer Science\5. Graphs"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\7. Computer Science\6. Computer Architecture"
+
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\8. Career Development"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\8. Career Development\1. Intro to Careera and the Tech Landscape"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\8. Career Development\2. Career Development I"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\8. Career Development\3. Career Development II"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\8. Career Development\4. Career Development III"
+
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\9. Lambda Next"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\9. Lambda Next\1. Next Week I"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\9. Lambda Next\2. Next Week II"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\9. Lambda Next\3. Next Week III"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\9. Lambda Next\4. Next Week IV"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\9. Lambda Next\5. Next Week V"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\9. Lambda Next\6. Next Week VI"
+
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\10. Computer Science Annex"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\10. Computer Science Annex\1. Data Structures - Heaps"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\10. Computer Science Annex\2. Intro to Django"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\10. Computer Science Annex\3. Python-Django I"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\10. Computer Science Annex\4. Python-Django II - Career Development"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\10. Computer Science Annex\5. Python-Django III - Career Development"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\10. Computer Science Annex\6. Project Week - Cellular Automata"
+MkDir "%USERPROFILE%\Desktop\Lambda\Couse\10. Computer Science Annex\7. C Programming Topics"
